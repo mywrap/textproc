0929fc8ff24cce2ffd381d52cb7f5a9b81ab509e
Đào Thanh Tùng <tung.dao@techx.vn>
2021-04-20 01:05:39 +0700

    add example GenRandomWord

--- example/example.go
+++ example/example.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"strings"
+
+	"github.com/mywrap/textproc"
+)
+
 func main() {
 	// see `text_test.go` and `html_test.go` for example
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(strings.ToLower(textproc.GenRandomWord(
+			8, 12, textproc.AlphaEnList)))
+	}
 }
